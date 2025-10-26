297b5a59d8629286b56c849b50e29bd76c5829ee
Yishu Goyal <yishugoyalbth@gmail.com>
2025-10-26 10:53:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  // Handle CORS preflight
+  
   if (req.method === "OPTIONS") {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
